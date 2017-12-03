8d77e158cd0ab003ee10dd37232820ad9fffa44e
Vitali Vishneuski <vishne.lospelicano@gmail.com>
2017-12-04 02:18:42 +0300

    Day 1-2

---
+++ adventofcode/adventofcode/Day1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace adventofcode
+{
+    public static class Day1
+    {
+        public static IEnumerable<int> ParseSequence(string sequence)
+        {
+            return sequence.Select(c => (int)char.GetNumericValue(c));
+        }
+
+        private static LinkedListNode<int> GetNext(LinkedListNode<int> node, int step = 1)
+        {
+            for (var i = 0; i < step; i++)
+            {
+                node = node.Next ?? node.List.First;
+            }
+            return node;
+        }
+
+        public static int GetSum(string sequence, int step = 1)
+        {
+            var list = new LinkedList<int>(ParseSequence(sequence));
+            var node = list.First;
+
+            var sum = 0;
+            do
+            {
+                if (node.Value == GetNext(node, step).Value)
+                    sum += node.Value;
+                node = GetNext(node);
+            } while (node != list.First);
+
+            return sum;
+        }
+
+        private static int GetStep(string sequence)
+        {
+            return sequence.Length / 2;
+        }
+
+        public static int GetSumWithStep(string sequence)
+        {
+            return GetSum(sequence, GetStep(sequence));
+        }
+    }
+}
---
+++ adventofcode/adventofcode/Day2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace adventofcode
+{
+    public static class Day2
+    {
+        public static IEnumerable<IEnumerable<int>> ParseSpreadsheet(string spreadsheet)
+        {
+            return spreadsheet.Split(new[] {Environment.NewLine}, StringSplitOptions.None)
+                              .Select(str => str.Split(new[] {" ", "\t"}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+        }
+
+        public static int GetCheckSum(string spreadsheet, Func<IEnumerable<int>, int> checkSumFunction)
+        {
+            return ParseSpreadsheet(spreadsheet).Sum(checkSumFunction);
+        }
+
+        public static int GetDifference(IEnumerable<int> row)
+        {
+            var max = int.MinValue;
+            var min = int.MaxValue;
+            foreach (var value in row)
+            {
+                if (max < value) max = value;
+                if (min > value) min = value;
+            }
+            var difference = max - min;
+            return difference;
+        }
+
+        public static int GetDivision(IEnumerable<int> row)
+        {
+            var list = row.ToList();
+            for (var i = 0; i < list.Count; i++)
+            {
+                var ii = list[i];
+                for (var j = i+1; j < list.Count; j++)
+                {
+                    var jj = list[j];
+                    if (ii % jj == 0)
+                        return ii / jj;
+                    if (jj % ii == 0)
+                        return jj / ii;
+                }
+            }
+            return 0;
+        }
+    }
+}
