e44479e443f968acc842cf6c03d989f66d47dc09
Vitali Vishneuski <vishne.lospelicano@gmail.com>
2017-12-05 00:44:09 +0300

    Day3

---
+++ adventofcode/adventofcode.test/Day3Test.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace adventofcode.test
+{
+    [TestFixture]
+    public class Day3Test
+    {
+        [TestCase(1, ExpectedResult = 0)]
+        [TestCase(2, ExpectedResult = 1)]
+        [TestCase(3, ExpectedResult = 2)]
+        [TestCase(4, ExpectedResult = 1)]
+        [TestCase(5, ExpectedResult = 2)]
+        [TestCase(6, ExpectedResult = 1)]
+        [TestCase(7, ExpectedResult = 2)]
+        [TestCase(8, ExpectedResult = 1)]
+        [TestCase(9, ExpectedResult = 2)]
+        [TestCase(10, ExpectedResult = 3)]
+        [TestCase(11, ExpectedResult = 2)]
+        [TestCase(12, ExpectedResult = 3)]
+        [TestCase(20, ExpectedResult = 3)]
+        [TestCase(23, ExpectedResult = 2)]
+        [TestCase(33, ExpectedResult = 4)]
+        [TestCase(34, ExpectedResult = 3)]
+        [TestCase(35, ExpectedResult = 4)]
+        [TestCase(36, ExpectedResult = 5)]
+        [TestCase(37, ExpectedResult = 6)]
+        [TestCase(38, ExpectedResult = 5)]
+        [TestCase(39, ExpectedResult = 4)]
+        [TestCase(40, ExpectedResult = 3)]
+        [TestCase(50, ExpectedResult = 7)]
+        [TestCase(1024, ExpectedResult = 31)]
+        [TestCase(277678, ExpectedResult = 475)]
+        public int GetDistance(int number)
+        {
+            return Day3.GetDistance(number);
+        }
+
+        [TestCase(277678, ExpectedResult = 59)]
+        public int GetFirstLarger(int number)
+        {
+            return Day3.GetFIrstLarger(number);
+        }
+    }
+}
---
+++ adventofcode/adventofcode/Day3.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace adventofcode
+{
+    public static class Day3
+    {
+        private static readonly IList<Func<int, int, int>> Tools =
+            new List<Func<int, int, int>> {GetLegDistance, GetHandDistance};
+
+        public static int GetDistance(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    return 0;
+                case 2:
+                    return 1;
+                default:
+                {
+                    var root = GetRoot(number);
+
+                    if (GetBranch(root) == number)
+                        return root - 1;
+
+                    var toolIndex = number / GetBranch(root);
+                    return Tools[toolIndex](number, root);
+                }
+            }
+        }
+
+        private static int GetLegDistance(int number, int root)
+        {
+            var knee = root / 2 + 1;
+            var leg = GetBranch(root) - number;
+
+            if (leg <= root - knee)
+                return root - 1 - leg;
+
+            return leg + GetLeaf(root);
+        }
+
+        private static int GetHandDistance(int number, int root)
+        {
+            var elbow = root / 2 + 1;
+            var hand = number - GetBranch(root);
+
+            if (hand <= elbow)
+                return root + 1 - hand;
+
+            return hand - GetLeaf(root);
+        }
+
+        private static int GetLeaf(int root)
+        {
+            return root % 2 == 0 ? 1 : 0;
+        }
+
+        private static int GetBranch(int root)
+        {
+            return (int) Math.Pow(root, 2);
+        }
+
+        private static int GetRoot(int number)
+        {
+            return (int) Math.Round(Math.Sqrt(number));
+        }
+
+        public static int GetFIrstLarger(int number)
+        {
+            return number;
+        }
+    }
+}
