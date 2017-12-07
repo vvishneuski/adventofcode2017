3aaed324e0a96e9f64378c4eec007e1df623deb0
Vitali Vishneuski <vishne.lospelicano@gmail.com>
2017-12-07 23:12:04 +0300

    Day6

---
+++ adventofcode/adventofcode.test/Day6Test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace adventofcode.test
+{
+    [TestFixture]
+    public class Day6Test
+    {
+        [TestCase("0 2 7 0", ExpectedResult = new[] { 0, 2, 7, 0 })]
+        public int[] ParseBanks(string banks)
+        {
+            return Day6.ParseBanks(banks);
+        }
+
+        [TestCase(new[] { 0, 2, 0, 0 }, 2, 7, ExpectedResult = new[] { 2, 4, 1, 2 })]
+        public int[] Redistribute(int[] banks, int index, int value)
+        {
+            return Day6.Redistribute(banks, index, value);
+        }
+
+        [TestCase(new[] { 0, 2, 7, 0 }, ExpectedResult = new[] { 2, 4, 1, 2 })]
+        public int[] FindAndRedistribute(int[] banks)
+        {
+            return Day6.FindMaxAndRedistribute(banks);
+        }
+
+        [TestCase("0 2 7 0", ExpectedResult = 5)]
+        [TestCase("4 1 15 12 0 9 9 5 5 8 7 3 14 5 12 3", ExpectedResult = 6681)]
+        public int NumberOfRedistribution(string banks)
+        {
+            return Day6.NumberOfRedistribution(banks);
+        }
+
+        [TestCase("0 2 7 0", ExpectedResult = 4)]
+        [TestCase("4 1 15 12 0 9 9 5 5 8 7 3 14 5 12 3", ExpectedResult = 2392)]
+        public int SizeOfInfiniteLoop(string banks)
+        {
+            return Day6.SizeOfInfiniteLoop(banks);
+        }
+    }
+}
---
+++ adventofcode/adventofcode/Day6.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace adventofcode
+{
+    public static class Day6
+    {
+        public static int NumberOfRedistribution(string _banks)
+        {
+            var banks = ParseBanks(_banks);
+            var states = new HashSet<string>();
+
+            var numberOfRedistribution = GetInfiniteLoop(states, banks);
+
+            return numberOfRedistribution;
+        }
+
+        private static int GetInfiniteLoop(ISet<string> states, int[] banks)
+        {
+            var numberOfRedistribution = 0;
+            while (!states.Contains(GetHash(banks)))
+            {
+                numberOfRedistribution++;
+                states.Add(GetHash(banks));
+                FindMaxAndRedistribute(banks);
+            }
+            return numberOfRedistribution;
+        }
+
+        public static int SizeOfInfiniteLoop(string _banks)
+        {
+            var banks = ParseBanks(_banks);
+            var states = new HashSet<string>();
+
+            GetInfiniteLoop(states, banks);
+
+            return states.SkipWhile(hash => !hash.Equals(GetHash(banks))).Count();
+        }
+
+        private static string GetHash(IEnumerable<int> banks)
+        {
+            return string.Join(" ", banks);
+        }
+
+        public static int[] ParseBanks(string banks)
+        {
+            return banks.Split(' ', '\t').Select(int.Parse).ToArray();
+        }
+
+        public static int[] Redistribute(int[] banks, int index, int value)
+        {
+            while (value-- > 0)
+            {
+                if (++index >= banks.Length)
+                {
+                    index = 0;
+                }
+
+                banks[index]++;
+            }
+
+            return banks;
+        }
+
+        public static int[] FindMaxAndRedistribute(int[] banks)
+        {
+            var maxIndex = 0;
+
+            for (var i = 1; i < banks.Length; i++)
+            {
+                if (banks[maxIndex] < banks[i])
+                {
+                    maxIndex = i;
+                }
+            }
+
+            var maxValue = banks[maxIndex];
+            banks[maxIndex] = 0;
+
+            return Redistribute(banks, maxIndex, maxValue);
+        }
+    }
+}
